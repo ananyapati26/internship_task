90c8777af5ac2e2c2b04ec28a4b8518bc55d5303
ananyapati26 <ananyaa.pati26@gmail.com>
2024-06-04 00:26:56 +0530

    changes

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -1,16 +1,15 @@
-import React from "react";
+import React from 'react'
+import { motion } from 'framer-motion'
 
 const Faq = () => {
   return (
     <>
-      <div className="xl:w-[70%] mx-auto mt-14">
+     <motion.div initial={{ y: 100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}  
+      transition={{ duration: 1 }}  className="xl:w-[70%] mx-auto mt-14">
         <div className="pl-20 max-w-[497px] flex flex-col gap-1">
-          <div className="text-[18px] text-[#FF5555] leading-[28px] font-medium">
-            FAQ
-          </div>
-          <div className=" text-[48px] leading-[48px] font-bold">
-            Frequently Asked Questions
-          </div>
+          <div className="text-[18px] text-[#FF5555] leading-[28px] font-medium">FAQ</div>
+          <div className=" text-[48px] leading-[48px] font-bold">Frequently Asked Questions</div>
         </div>
         <div className="w-full flex flex-wrap justify-center gap-5 my-10">
           <div className="w-full max-w-[534px] flex flex-col justify-center items-center gap-7">
@@ -18,9 +17,8 @@ const Faq = () => {
               <div className="text-[28px] leading-[28px] font-semibold capitalize">
                 the best financial accounting app ever!
               </div>
-              <div className="text-[18px] leading-[28px] font-medium">
-                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
-                ultricies. In ultrices malesuada elit mauris.
+              <div className="  text-[18px] leading-[28px] font-medium " >
+                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris.
               </div>
             </div>
 
@@ -28,9 +26,8 @@ const Faq = () => {
               <div className="text-[28px] leading-[28px] font-semibold capitalize">
                 the best financial accounting app ever!
               </div>
-              <div className="text-[18px] leading-[28px] font-medium">
-                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
-                ultricies. In ultrices malesuada elit mauris.
+              <div className="text-customGray text-[18px] leading-[28px] font-medium">
+                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris.
               </div>
             </div>
 
@@ -38,9 +35,8 @@ const Faq = () => {
               <div className="text-[28px] leading-[28px] font-semibold capitalize">
                 the best financial accounting app ever!
               </div>
-              <div className="text-[18px] leading-[28px] font-medium">
-                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
-                ultricies. In ultrices malesuada elit mauris.
+              <div className=" text-[18px] leading-[28px] font-medium">
+                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris.
               </div>
             </div>
           </div>
@@ -50,9 +46,8 @@ const Faq = () => {
               <div className="text-[28px] leading-[28px] font-semibold capitalize">
                 the best financial accounting app ever!
               </div>
-              <div className="text-[18px] leading-[28px] font-medium">
-                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
-                ultricies. In ultrices malesuada elit mauris.
+              <div className="text-customGray text-[18px] leading-[28px] font-medium">
+                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris.
               </div>
             </div>
 
@@ -61,8 +56,7 @@ const Faq = () => {
                 the best financial accounting app ever!
               </div>
               <div className="text-[18px] leading-[28px] font-medium">
-                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
-                ultricies. In ultrices malesuada elit mauris.
+                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris.
               </div>
             </div>
 
@@ -70,16 +64,13 @@ const Faq = () => {
               <div className="text-[28px] leading-[28px] font-semibold capitalize">
                 the best financial accounting app ever!
               </div>
-              <div className="text-[18px] leading-[28px] font-medium">
-                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
-                ultricies. In ultrices malesuada elit mauris.
+              <div className="text-customGray text-[18px] leading-[28px] font-medium">
+                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris.
               </div>
             </div>
           </div>
-        </div>
-      </div>
-    </>
-  );
-};
+        </div></motion.div></>
+  )
+}
 
-export default Faq;
+export default Faq
