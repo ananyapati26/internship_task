33755945a70d4c3041fa682d3bd572dca69e3a92
ananyapati26 <ananyaa.pati26@gmail.com>
2024-06-04 00:36:08 +0530

    changes

--- src/pages/Advertisement.jsx
+++ src/pages/Advertisement.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Frame from '../assets/advertise.png'
+import { motion } from 'framer-motion'
 
 const Advertisement = () => {
     return (
         <>
-            <div className="md:w-11/12 md:max-w-[1090px] md:mx-auto md:py-[100px] md:relative">
+            <motion.div initial={{ y: 100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}  
+      transition={{ duration: 1 }} className="md:w-11/12 md:max-w-[1090px] md:mx-auto md:py-[100px] md:relative">
                 <img src={Frame} alt='ok' className="w-[90%] pl-12 md:pl-0 md:w-full md:z-1" />
-            </div>
+            </motion.div>
 
         </>
     )
