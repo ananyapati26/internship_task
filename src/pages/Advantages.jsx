80aa6ce93d70c554acca3f43ce5790353264627a
ananyapati26 <ananyaa.pati26@gmail.com>
2024-06-04 00:23:50 +0530

    changes

--- src/pages/Advantages.jsx
+++ src/pages/Advantages.jsx
@@ -3,12 +3,15 @@ import group2 from '../assets/Group2.svg'
 import frame4 from '../assets/Frame 4.svg'
 import frame8 from '../assets/Frame 8.svg'
 import group3 from '../assets/Group3.svg'
+import { motion } from 'framer-motion'
 
 const Advantages = () => {
   return (
     <>
     <div  className=' lg:w-[70%] flex flex-col w-[100%] justify-center items-center lg:flex lg:flex-row lg:justify-center lg:items-center'>
-        <div className='lg:w-[50%] flex flex-col justify-center pl-20 pr-20 xl:pr-0'>
+        <motion.div initial={{ opacity: 0, x: 0 }}
+          animate={{ opacity: 1, x: 90 }}
+          transition={{ duration: 1 }} className='lg:w-[50%] flex flex-col justify-center pl-20 pr-20 xl:pr-0'>
             <div><h3 className='text-[18px] tracking-[1.6px] text-[#FF5555] mb-[4px] xl:text-[16px]'>
               ADVANTAGES
             </h3></div>
@@ -28,11 +31,15 @@ const Advantages = () => {
               rhoncus, diam suspendisse sit nunc, gravida eu. Lectus eget eget
               ac dolor neque lorem sapien, suspendisse aliquam.
             </p></div>
-        </div>
-        <div className='w-[80%] lg:w-[50%] '><img src={frame4} alt='ok'/></div>
+        </motion.div>
+        <div className='w-[80%] lg:w-[50%] '><motion.img initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 1 }} src={frame4} alt='ok'/></div>
     </div>
     <div  className='lg:w-[70%] flex flex-col w-[100%] justify-center items-center lg:flex lg:flex-row-reverse lg:justify-center lg:items-center'>
-    <div className='lg:w-[50%] flex flex-col justify-center pl-20 pr-20 lg:pr-20 lg:pl-0'>
+    <motion.div initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }} className='lg:w-[50%] flex flex-col justify-center pl-20 pr-20 lg:pr-20 lg:pl-0'>
         
         <div className='flex items-center gap-[15px] pb-5'>
             <div><img src={group3} alt='advantage' /></div>
@@ -47,8 +54,10 @@ const Advantages = () => {
           rhoncus, diam suspendisse sit nunc, gravida eu. Lectus eget eget
           ac dolor neque lorem sapien, suspendisse aliquam.
         </p></div>
-    </div>
-    <div className='w-[80%] lg:w-[50%]'><img src={frame8} alt='ok'/></div>
+    </motion.div>
+    <div className='w-[80%] lg:w-[50%]'><motion.img initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 1 }} src={frame8} alt='ok'/></div>
 </div>
 </>
   )
